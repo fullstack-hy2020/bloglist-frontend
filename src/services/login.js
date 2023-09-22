86f44a04fa88a89cfc0298ccd62ac48c0e0611ce
Kohdy Nicholson <kohdy.nicholson@varian.com>
2023-09-22 14:13:05 +0300

    feat[5.1]: login service for returning user data

---
+++ src/services/login.js
@@ -0,0 +1,17 @@
+import axios from 'axios'
+
+const baseUrl = '/api/login'
+
+const login = async (username, password) => {
+  const credentials = {
+    username,
+    password
+  }
+
+  const response = await axios
+    .post(baseUrl, credentials)
+
+  return response.data
+}
+
+export default { login }
