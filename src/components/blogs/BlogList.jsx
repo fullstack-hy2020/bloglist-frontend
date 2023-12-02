1c201fe48c54b9df42b06e6a8630dd83a24179fb
Kohdy Nicholson <kohdy.nicholson@varian.com>
2023-12-03 01:13:41 +0200

    feat{7.16]: remove blog functionality from BlogList component

--- src/components/blogs/BlogList.jsx
+++ src/components/blogs/BlogList.jsx
@@ -1,61 +1,37 @@
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
-import _ from "lodash";
-import Blog from "./Blog";
 import BlogForm from "./BlogForm";
-import { remove, like, getAll } from "./reducers/blogsReducer";
-import {
-  useNotificationDispatch,
-  showNotification,
-} from "../shared/contexts/NotificationContext";
+import { getAll } from "./reducers/blogsReducer";
 
 const BlogList = ({ blogs }) => {
   const dispatch = useDispatch();
-  const notificationDispatch = useNotificationDispatch();
 
   useEffect(() => {
     dispatch(getAll());
   }, []);
 
-  const deleteBlog = (blog) => () => {
-    if (window.confirm(`Remove blog ${blog.title} by ${blog.author}?`)) {
-      dispatch(remove(blog))
-        .then(() => {
-          showNotification(notificationDispatch, {
-            message: "Blog deleted successfully.",
-            type: "success",
-          });
-        })
-        .catch((error) => {
-          showNotification(notificationDispatch, {
-            message: error,
-            type: "error",
-          });
-        });
-    }
+  const style = {
+    paddingTop: 5,
+    paddingBottom: 5,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
   };
-
-  const likeBlog = (blog) => () => dispatch(like(blog));
 
   return (
     <div>
       <h2>blogs</h2>
       <BlogForm />
       <h3>Saved Blogs</h3>
-      <div>
-        {blogs.map((blog) => (
+      {blogs.map((blog) => (
+        <div style={style}>
           <Link to={`/blogs/${blog.id}`}>
             {blog.title} {blog.author}
           </Link>
-          // <Blog
-          //   key={blog.id}
-          //   blog={blog}
-          //   deleteBlog={deleteBlog}
-          //   likeBlog={likeBlog}
-          // />
-        ))}
-      </div>
+        </div>
+      ))}
     </div>
   );
 };
