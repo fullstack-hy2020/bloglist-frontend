5f6cd33d59c5cbf60e1a28bf40705ad375e1051c
Anni Parkkila <anni.parkkila@kaikuhealth.com>
2024-08-24 21:16:15 +0300

    Step10 like-sorttaus

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ const App = () => {
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
-      setBlogs( blogs )
+      setBlogs(blogs)
     )
   }, [])
 
@@ -142,8 +142,8 @@ const App = () => {
         <BlogForm createBlog={addBlog} />
       </Togglable>
       <div className='bloglist'>
-        {blogs.map(blog =>
-          <Blog key={blog.id} blog={blog} updateLikes={() => updateLikes(blog)}/>
+        {blogs.sort((a, b) => b.likes - a.likes).map(blog =>
+          <Blog key={blog.id} blog={blog} updateLikes={() => updateLikes(blog)} removeBlog={() => removeBlog(blog.id)}/>
         )}
       </div>
     </div>
